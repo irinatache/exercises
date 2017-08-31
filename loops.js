009a90cfa0c5900c68a01381714da0eecbe6732c
irinatache <tache_irina@yahoo.com>
2017-09-01 00:12:10 +1000

    loops play

---
+++ loops.js
@@ -0,0 +1,20 @@
+var x = 1;
+while (x <= 10) {
+  console.log(x);
+  x++;
+}
+
+for (y = 1; y <= 10; y++) {
+  console.log(y);
+}
+
+var z = 1;
+do {
+  console.log(z);
+  z++;
+} while (z <= 10);
+
+// do:                   while
+// 1 variable            1 variable
+// 2 actions             2 condition
+// 3 condition           3 actions
